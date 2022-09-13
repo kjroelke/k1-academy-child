c99aaa1b738546b12a3250e58d39c7225c665fe2
Joontae93 <kj.roelke@gmail.com>
2022-09-13 18:38:24 -0500

    build course form and style it

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '5f8af268551fbf04ddb3');
+<?php return array('dependencies' => array(), 'version' => '637f3a17dd19fd3b3ce2');
--- template-custom-sales.php
+++ template-custom-sales.php
@@ -8,32 +8,66 @@
 get_header();
 ?>
 <div class="x-container max width">
+    <? the_content(); ?>
     <header>
         <h1>Hello there.</h1><span>Let's Get Started.</span>
     </header>
     <form class="sales-form">
+        <div class="the-user">
+            <div class="the-user__name">
+                <label for="user-first-name">First Name</label>
+                <input required type="text" name="user-first-name" id="user-first-name" placeholder="Steven">
+                <label for="user-last-name">Last Name</label>
+                <input required type="text" name="user-last-name" id="user-last-name" placeholder="Bush">
+            </div>
+            <div class="the-user__email">
+                <label for="user-email">Your email</label>
+                <input type="email" name="email" id="user-email" placeholder="hello@kingdomone.co">
+            </div>
+        </div>
         <div class="the-organizaton">
+            <label for="org-type">
+                <h2>What type of organization are you?</h2>
+            </label>
+            <select required name="org-type" id="org-type">
+                <option value="Church">Church</option>
+                <option value="School">School</option>
+                <option value="Non-Profit Organization">Non-Profit Organization</option>
+            </select>
             <label for="org-name">
                 <h2>What's the name of your organization?</h2>
             </label>
-            <input type="text" id="org-name">
+            <input required type="text" id="org-name">
+
+        </div>
+        <div class="the-seats">
+            <div class="licenses">
+                <label for="employee--full">
+                    <span>How many <strong>full-time employees</strong> do you have that need access?</span>
+                </label>
+                <input type="number" id="employee--full" required>
+            </div>
+            <div class="licenses"><label for="employee--part">
+                    <span>How many <strong>part-time employees</strong> do you have that need access?</span>
+                </label>
+                <input type="number" id="employee--part" required>
+            </div>
+            <div class="licenses"><label for="volunteers">
+                    <span>How many <strong>volunteers</strong> do you have that need access?</span>
+                </label>
+                <input type="number" id="volunteers" required>
+            </div>
         </div>
         <div class="course-container">
             <label for="courses">
                 <h2>Select which courses you would like to purchase.</h2>
             </label>
             <div class="the-courses"></div>
-            <div class="the-seats">
-                <label for="seats">
-                    <h2>How many licenses do you need?</h2>
-                </label>
-                <input type="number" id="seats" required="">
-            </div>
-            <input type="submit" value="Submit">
-
-            <div id="custom-sales-form">This page requires Javascript to work. If you need help, please contact us at <a
-                    href="mailto:hello@kingdomone.co">hello@kingdomone.co</a></div>
         </div>
+        <div class="the-price"></div>
+        <input type="submit" value="Submit">
     </form>
+    <div id="custom-sales-form">This page requires Javascript to work. If you need help, please contact us at <a
+            href="mailto:hello@kingdomone.co">hello@kingdomone.co</a></div>
 </div>
 <? get_footer(); ?>
