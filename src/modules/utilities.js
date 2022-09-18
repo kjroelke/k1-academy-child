eb715ad50958e79a6b78f70a24977ce65ea7e75e
Joontae93 <kj.roelke@gmail.com>
2022-09-18 15:38:37 -0500

    refactor myCopyright, API_URL, define makeRequest

--- src/modules/utilities.js
+++ src/modules/utilities.js
@@ -21,8 +21,42 @@ export function getFormVal(selector) {
  */
 export function myCopyright(brandName) {
 	const copyright = document.getElementById('copyright');
-	const thisYear = new Date().getFullYear();
-	copyright.innerHTML = `<p>&copy; ${thisYear} ${brandName} All Rights Reserved.`;
+	copyright.innerHTML = `<p>&copy; ${k1AcademyData.year} ${brandName} All Rights Reserved.`;
 }
 
-export const API_URL = `https://k1academy.local/wp-json/llms/v1`;
+export const API_URL = `${k1AcademyData.root_url}/wp-json/llms/v1/`;
+
+/**
+ * Makes AJAX request to LMS API. Also converts `'accessPlans'` to HTML-friedly `'access-plans.'`
+ * @param {string} endpoint the endpoint url to add. *Note: should not include leading '/'*
+ * @param {string} method the AJAX Method (GET, POST, DELETE, UPDATE)
+ * @param {boolean} returnAll if `true`, returns an Array, else only return the `data`
+ * @returns {Array|Object} `data` object or an Array containing [AJAX `res`ponse, The `data`, The `method`]
+ */
+export async function makeRequest(
+	endpoint,
+	method = 'GET',
+	theData = false,
+	returnAll = false,
+) {
+	endpoint = endpoint === 'accessPlans' ? 'access-plans' : endpoint;
+	try {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				'X-LLMS-CONSUMER-KEY': process.env.API_KEY,
+				'X-LLMS-CONSUMER-SECRET': process.env.API_SECRET,
+			},
+			method: `${method}`,
+			timeout: 5000,
+		};
+		if (theData) {
+			config.body = JSON.stringify(theData);
+		}
+		console.log(config);
+		const res = await fetch(API_URL + `${endpoint}`, config);
+		const data = await res.json();
+		if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+		return returnAll ? [res, data, method] : data;
+	} catch (error) {}
+}
